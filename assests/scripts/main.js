488d12a7aea1828437187f630dffaf78a255cbda
Aditya Singhal <54800695+speedychital@users.noreply.github.com>
2020-06-27 21:00:31 +0530

    Added files

---
+++ assests/scripts/main.js
@@ -0,0 +1,14 @@
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "150px";
+  document.getElementById("main").style.marginRight = "150px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginRight= "0";
+}
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
